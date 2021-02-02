f7658b1b7412c0cd8d46e32264e48f3142d848fa
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2021-02-02 21:25:57 +0900

    Ts 타입 추론 써도 되나?

---
+++ typescript/1-TYPES/1-9-inference.ts
@@ -0,0 +1,15 @@
+{
+    //Type Inference
+    let text: string = 'hello';
+
+    function print(message: string) {
+        console.log(message);
+    }
+
+    //return number
+    function add(x: number, y: number) {
+        return x + y;
+    }
+    //number
+    const result = add(1, 2)
+}
