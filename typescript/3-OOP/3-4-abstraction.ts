8f3b65047a8a9f459029c1725e792e3bcde94f4f
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2021-02-08 20:38:25 +0900

    Ts 추상화 정리

--- typescript/3-OOP/3-4-abstraction.ts
+++ typescript/3-OOP/3-4-abstraction.ts
@@ -6,67 +6,52 @@
 
     //public
     //private
-    //protected 상속 후 에도 접근 가능 하자 외부에서는 접근 불가능
+    //protected 자식 클래스에 접근 가능
 
     interface CoffeeMaker {
-        makeCoffee(shots: number): CoffeeCup;
+        makerCoffee(shots: number): CoffeeCup;
     }
 
-    interface CommercialCoffeeMaker {
-        makeCoffee(shots: number): CoffeeCup;
-        fillCoffeeBeans(beans: number): void;
-        clean(): void;
-
-    }
-
-    class CoffeeMachine implements CoffeeMaker, CommercialCoffeeMaker {
-        private static BEANS_GRAMM_PER_SHOT: number = 7; // class level
-        private coffeeBeans: number = 0; // instance (object) level
+    class CoffeeMachine implements CoffeeMaker {
+        private static BEANS_GRAMM_PER_SHOT:number = 7;
+        private coffeeBeans: number = 0; //instance (object) level
 
         private constructor(coffeeBeans: number) {
-            this.coffeeBeans = coffeeBeans;
+            this.coffeeBeans = coffeeBeans
         }
 
-        static makerMatchine(coffeeBeans: number): CoffeeMaker {
+        static makerMachine(coffeeBeans: number): CoffeeMachine {
             return new CoffeeMachine(coffeeBeans);
         }
 
-        // makerMatchine(coffeeBeans: number): CoffeeMaker {
-        //     return new CoffeeMaker(coffeeBeans);
-        // }
-
-        fillCoffeeBeans(beans: number) {
+        filleCoffeeBeans(beans: number) {
             if(beans < 0) {
                 throw new Error('value for beans should be greater than 0');
             }
             this.coffeeBeans += beans;
         }
 
-        clean() {
-            console.log('cleaning the machine...');
-        }
-
         private grindBeans(shots: number) {
-            console.log(`Bean for ${shots}`);
-            if(this.coffeeBeans < shots *  CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
+            console.log(`grinding beans for ${shots}`);
+            if(this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
                 throw new Error('Not enough coffee beans!');
             }
             this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
         }
 
-        private preheat(): void {
-            console.log('heating up...');
+        private preheat() {
+            console.log('Heating Heating~~!!!');
         }
 
-        private extract(shots: number): CoffeeCup {
-            console.log(`Pulling ${shots}`);
+        private extract(shots: number):CoffeeCup {
+            console.log(`Pulling Up ${shots}...`);
             return {
                 shots,
                 hasMilk: false
             }
         }
 
-        makeCoffee(shots: number): CoffeeCup {
+        makerCoffee(shots: number):CoffeeCup {
             this.grindBeans(shots);
             this.preheat();
             return this.extract(shots);
@@ -76,16 +61,15 @@
             // this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
             // return {
             //     shots,
-            //     hasMilk: false
-            // };
+            //     hasMilk: false,
+            // }
         }
     }
+    const maker:CoffeeMachine = CoffeeMachine.makerMachine(32);
+    maker.filleCoffeeBeans(17);
+    maker.makerCoffee(2);
 
-     const maker: CoffeeMachine = CoffeeMachine.makerMatchine(32);
-     maker.fillCoffeeBeans(32);
-     maker.makeCoffee(2);
-
-     const maker2: CommercialCoffeeMaker = CoffeeMachine.makerMatchine(32);
-     maker2.fillCoffeeBeans(32);
-     maker2.makeCoffee(2);
+    const maker2:CoffeeMaker = CoffeeMachine.makerMachine(32);
+    //maker2.filleCoffeeBeans(17);
+    maker2.makerCoffee(2);
 }
