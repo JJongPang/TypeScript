a716853dd69a190e8777b0072cd8d2f4874c5664
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2021-02-21 19:13:13 +0900

    Ts enum 타입 재정리 재복습

--- typescript/1-TYPES/1-8-enum.ts
+++ typescript/1-TYPES/1-8-enum.ts
@@ -1,26 +1,33 @@
 {
-    //Enum 상수값을 한 곳으로 모아 정리하는 것, 타입 보장 
+    // Enum: 여러가지에 관련된 상수값들을 한 곳으로 모아서 정의할수있게 도와주는 타입
 
-    //JavaScript
+
+    // JavaScript
     const MAX_NUM = 6;
-    const Max_SUTDENT_PER_CLASS = 10;
+    const MAX_STUDENTS_PER_CLASS = 10;
     const MONDAY = 0;
     const TUESDAY = 1;
     const WEDNESDAY = 2;
-    const DAYS_ENUM = Object.freeze({"MONDAY": 0, "TUESDAY": 1, "WEDESDAY": 2});
+    const DAYS_ENUM = Object.freeze({"MONDAY": 0, "TUESDAY": 2, "WEDNESDAY": 3});
     const dayOfToday = DAYS_ENUM.MONDAY;
 
-    //TypeScript
-    enum Days {
-        Monday,
-        Tesday,
-        Wednesday,
-        Thuresday,
-        Friday,
-        Satarday,
-        Sunday
-    }
+    // TypeScript
+    // 되도록이면 union type을 사용하면 좋다.
+     type DaysOfWeek = 'Monday' | 'Tuesday' | 'Wednesday';
 
-    //enum대신 uniontype 사용이 수월
-    type DaysOfWeek = 'Monday' | 'Tuesday' | 'Wednesday';
+    enum Days {
+        Monday = 'mon',
+        Tuesday = 'tue',
+        Wednesday = 'wed',
+        Thursday = 'thu',
+        Friday = 'fri',
+        Satarday = 'sat',
+        Sunday = 'sun',
+    }
+    console.log(Days.Monday);
+    const day: Days = Days.Satarday;
+    console.log(day);
+
+    const dayOfResult: DaysOfWeek = 'Monday';
+
 }
