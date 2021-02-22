081d22829f1a5099e9c3a44dcf4a5936384ae4c4
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2021-02-22 21:51:40 +0900

    Ts 객체지향적으로 커피기계만들기

--- typescript/3-OOP/3-2-class.ts
+++ typescript/3-OOP/3-2-class.ts
@@ -5,22 +5,22 @@
     }
 
     class CoffeeMaker {
-        private static BEANS_GRAMM_PER_SHOT: number = 7;
-        private coffeeBenas: number = 0;
-
-        constructor(coffeeBeans: number){
-            this.coffeeBenas = coffeeBeans;
+        static BEANS_GRAMM_PER_SHOT: number = 7 //class level
+        coffeeBeans: number = 0; // instance(object) level
+        
+        constructor(coffeeBeans: number) {
+            this.coffeeBeans = coffeeBeans;
         }
 
         static makeMachine(coffeeBeans: number): CoffeeMaker {
             return new CoffeeMaker(coffeeBeans);
         }
 
-        makeCoffee(shots: number): CoffeeCup {
-            if(this.coffeeBenas < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
-                throw new Error("Not enough coffee beans");
+        makeCoffe(shots: number): CoffeeCup {
+            if(this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
+                throw new Error('Not enough coffee beans!');
             }
-            this.coffeeBenas -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
+            this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
             return {
                 shots,
                 hasMilk: false
@@ -28,6 +28,8 @@
         }
     }
 
-   const maker = new CoffeeMaker(32);
-   console.log(maker);
+    let coffee = new CoffeeMaker(32);
+    let maker = CoffeeMaker.makeMachine(22);
+    console.log(coffee);
+    console.log(maker);
 }
