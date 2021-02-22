973a201eec796ba1fb4305d44031933d19d09619
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2021-02-22 22:05:15 +0900

    Ts 객체지향적으로 만든 커피기계 캡슐화 작업 진행

--- typescript/3-OOP/3-3-encapsulation.ts
+++ typescript/3-OOP/3-3-encapsulation.ts
@@ -9,60 +9,38 @@
     // protected
 
     class CoffeeMaker {
-        private static BEANS_GRAMM_PER_SHOT: number = 7;
-        private coffeeBenas: number = 0;
-
-        private constructor(coffeeBeans: number){
-            this.coffeeBenas = coffeeBeans;
+        private static BEANS_GRAMM_PER_SHOT: number = 7 //class level
+        private coffeeBeans: number = 0; // instance(object) level
+        
+        private constructor(coffeeBeans: number) {
+            this.coffeeBeans = coffeeBeans;
         }
 
         static makeMachine(coffeeBeans: number): CoffeeMaker {
             return new CoffeeMaker(coffeeBeans);
         }
 
-        filleCoffeeBeans(beans: number) {
+        fillCoffeeBeans(beans: number) {
             if(beans < 0) {
-                throw new Error('value for beans should be greater than 0')
+                throw new Error('value for beans should be greater than 0');
             }
-            this.coffeeBenas += beans;
+            this.coffeeBeans += beans;
         }
 
-        makeCoffee(shots: number): CoffeeCup {
-            if(this.coffeeBenas < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
-                throw new Error("Not enough coffee beans");
+        makeCoffe(shots: number): CoffeeCup {
+            if(this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
+                throw new Error('Not enough coffee beans!');
             }
-            this.coffeeBenas -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
+            this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
             return {
                 shots,
                 hasMilk: false
             }
         }
     }
-    
-    class User {
-        get fullName ():string{
-            return `${this.firstName} ${this.lastName}`
-        } 
 
-        private internalAge = 4;
-        
-        get age(): number {
-            return this.internalAge;
-        }
+    let maker = CoffeeMaker.makeMachine(32);
+    maker.fillCoffeeBeans(32);
+    console.log(maker)
 
-        set age(num: number) {
-            if(num < 0) {
-                throw new Error('NO NO NO');
-            }
-            this.internalAge = num;
-        }
-
-        constructor(private firstName: string, private lastName: string) {
-            this.firstName = firstName;
-            this.lastName = lastName;
-        }
-    }
-
-    const user = new User('jong', 'hyeon');
-    console.log(user.fullName);
 }
