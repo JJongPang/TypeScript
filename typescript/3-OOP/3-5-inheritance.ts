cf21ea02b961e8dcf83c5c78e148a729f22ab292
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2021-02-23 23:03:00 +0900

    Ts  상속을 통한 다른 커피머신 만들기

--- typescript/3-OOP/3-5-inheritance.ts
+++ typescript/3-OOP/3-5-inheritance.ts
@@ -1,54 +1,52 @@
 {
     type CoffeeCup = {
         shots: number;
-        hasMilk: boolean;
+        hasMilk?: boolean;
     }
 
     interface CoffeeMaker {
         makeCoffee(shots: number): CoffeeCup;
     }
+    
+    
 
     class CoffeeMachine implements CoffeeMaker {
-        private static BEANS_GRAMM_PER_SHOT: number = 7;
-        private coffeeBenas: number = 0;
-
-        constructor(coffeeBeans: number){
-            this.coffeeBenas = coffeeBeans;
+        private static BEANS_GRAMM_PER_SHOT: number = 7 //class level
+        private coffeeBeans: number = 0; // instance(object) level
+        
+        constructor(coffeeBeans: number) {
+            this.coffeeBeans = coffeeBeans;
         }
 
         static makeMachine(coffeeBeans: number): CoffeeMachine {
             return new CoffeeMachine(coffeeBeans);
         }
 
-        filleCoffeeBeans(beans: number) {
+        fillCoffeeBeans(beans: number) {
             if(beans < 0) {
-                throw new Error('value for beans should be greater than 0')
+                throw new Error('value for beans should be greater than 0');
             }
-            this.coffeeBenas += beans;
-        }
-
-        clean() {
-            console.log('cleaning the machine....');
+            this.coffeeBeans += beans;
         }
 
         private grindBeans(shots: number) {
-            console.log(`grinding beans for ${shots}`);
-            if(this.coffeeBenas < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
-                throw new Error('Not enough coffee beans!');
+            console.log(`shots ++++${shots} 추가`);
+            if(this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
+                throw new Error('Not enough coffee beans~!!!');
             }
-            this.coffeeBenas -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
+            this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
         }
 
-         private preheat(): void {
-            console.log('HEATING UP... HEATING UP...');
+        private preheat() {
+            console.log('heating heating up');
         }
 
         private extract(shots: number): CoffeeCup {
-            console.log(`Pulling ${shots} shots...`);
+            console.log(`Pulling ${shots} shots`)
             return {
                 shots,
                 hasMilk: false
-            }
+            }    
         }
 
         makeCoffee(shots: number): CoffeeCup {
@@ -56,15 +54,19 @@
             this.preheat();
             return this.extract(shots);
         }
+
+        clean() {
+            console.log('cleaning the machine....');
+        }
     }
 
-    class CoffeLatteMachine extends CoffeeMachine {
-        constructor(beans: number, public readonly serialNumber: string) {
-            super(beans)    
+    class CaffeLatteMachine extends CoffeeMachine {
+        constructor(beans:number, public readonly seriaNumber: string) {
+            super(beans);
         }
 
         private steamMilk(): void {
-            console.log( 'Steaming some milk...');
+            console.log('steaming milk');
         }
 
         makeCoffee(shots: number): CoffeeCup {
@@ -75,10 +77,10 @@
                 hasMilk: true
             }
         }
-    } 
+    }
 
-    const machine = new CoffeeMachine(32);
-    const latteMachine = new CoffeLatteMachine(32, 'ssss');
-    const coffee = latteMachine.makeCoffee(1);
-    console.log(latteMachine.serialNumber);
+    const machine = new CoffeeMachine(23);
+    const latteMachine = new CaffeLatteMachine(23, '11111111');
+    const coffee = latteMachine.seriaNumber;
+    console.log(coffee);
 }
