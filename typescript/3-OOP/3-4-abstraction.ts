b6434a6dcd6bf15670bbac296982a74b301d8c65
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2021-02-23 22:20:42 +0900

    Ts 인터페이스로 재구현

--- typescript/3-OOP/3-4-abstraction.ts
+++ typescript/3-OOP/3-4-abstraction.ts
@@ -10,68 +10,60 @@
 
     interface CommercialCoffeeMaker {
         makeCoffee(shots: number): CoffeeCup;
-        filleCoffeeBeans(beans: number): void;
-        clean(): void;
+        fillCoffeeBeans(beans: number): void;
+        clean(): void; 
     }
 
     class CoffeeMachine implements CoffeeMaker, CommercialCoffeeMaker {
-        private static BEANS_GRAMM_PER_SHOT: number = 7;
-        private coffeeBenas: number = 0;
-
-        private constructor(coffeeBeans: number){
-            this.coffeeBenas = coffeeBeans;
+        private static BEANS_GRAMM_PER_SHOT: number = 7 //class level
+        private coffeeBeans: number = 0; // instance(object) level
+        
+        private constructor(coffeeBeans: number) {
+            this.coffeeBeans = coffeeBeans;
         }
 
         static makeMachine(coffeeBeans: number): CoffeeMachine {
             return new CoffeeMachine(coffeeBeans);
         }
 
-        filleCoffeeBeans(beans: number) {
+        fillCoffeeBeans(beans: number) {
             if(beans < 0) {
-                throw new Error('value for beans should be greater than 0')
+                throw new Error('value for beans should be greater than 0');
             }
-            this.coffeeBenas += beans;
-        }
-
-        clean() {
-            console.log('cleaning the machine....');
+            this.coffeeBeans += beans;
         }
 
         private grindBeans(shots: number) {
-            console.log(`grinding beans for ${shots}`);
-            if(this.coffeeBenas < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
-                throw new Error('Not enough coffee beans!');
+            console.log(`shots ++++${shots} 추가`);
+            if(this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
+                throw new Error('Not enough coffee beans~!!!');
             }
-            this.coffeeBenas -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
+            this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
         }
 
-         private preheat(): void {
-            console.log('HEATING UP... HEATING UP...');
+        private preheat() {
+            console.log('heating heating up');
         }
 
         private extract(shots: number): CoffeeCup {
-            console.log(`Pulling ${shots} shots...`);
+            console.log(`Pulling ${shots} shots`)
             return {
                 shots,
                 hasMilk: false
-            }
+            }    
         }
 
         makeCoffee(shots: number): CoffeeCup {
             this.grindBeans(shots);
             this.preheat();
             return this.extract(shots);
-            // if(this.coffeeBenas < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
-            //     throw new Error("Not enough coffee beans");
-            // }
-            // this.coffeeBenas -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
-            // return {
-            //     shots,
-            //     hasMilk: false
-            // }
+        }
+
+        clean() {
+            console.log('cleaning the machine....');
         }
     }
-    
+
     class AmateurUser {
         constructor(private machine: CoffeeMaker) {}
         makeCoffee() {
@@ -79,22 +71,22 @@
             console.log(coffee);
         }
     }
-    
-    class ProBrista {
+
+    class ProBarista {
         constructor(private machine: CommercialCoffeeMaker) {}
         makeCoffee() {
             const coffee = this.machine.makeCoffee(2);
             console.log(coffee);
-            this.machine.filleCoffeeBeans(45);
+            this.machine.fillCoffeeBeans(45);
             this.machine.clean();
         }
     }
+
     const maker: CoffeeMachine = CoffeeMachine.makeMachine(32);
+    console.log('maker', maker)
     const amateur = new AmateurUser(maker);
-    const pro = new ProBrista(maker);
-
+    const pro = new ProBarista(maker);
     amateur.makeCoffee();
-    console.log('-------------');
     pro.makeCoffee();
-
+    
 }
