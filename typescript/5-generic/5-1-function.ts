51db5880fc20e12b5d3d75bb0427e7a9d145a202
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2021-02-25 20:54:40 +0900

    Ts 함수에서 제네릭 재복습

--- typescript/5-generic/5-1-function.ts
+++ typescript/5-generic/5-1-function.ts
@@ -1,26 +1,24 @@
 {
     function checkNotNullBad(arg: number | null): number {
         if(arg == null) {
-            throw new Error('not valid number!');
+            throw new Error('Error');
         }
         return arg;
     }
-    
-    //any bad
-   function checkNotNullAnyBad(arg: any | null): any {
+
+    function checkNotAnyBad(arg: any | null): any {
         if(arg == null) {
-            throw new Error('not valid number!');
+            throw new Error('not valid number');
         }
         return arg;
     }
 
-   function checkNotNull<T>(arg: T | null): T {
-    if(arg == null) {
-        throw new Error('not valid number!');
+    function checkNotNumber<T>(arg: T | null): T {
+        if(arg == null) {
+            throw new Error('not valid number');
+        }
+        return arg;
     }
-    return arg;
-   }
-
-   const number: number = checkNotNull(123);
-   const boal: boolean = checkNotNull(true);
+    const number: number = checkNotNumber(123);
+    const boal: boolean = checkNotNumber(true);
 }
