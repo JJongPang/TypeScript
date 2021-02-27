e407f2378ed1ad7759c5781d1874b953941dc682
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2021-02-28 00:07:05 +0900

    Ts partial type  재복습

--- typescript/9-advance/9-6-partial.ts
+++ typescript/9-advance/9-6-partial.ts
@@ -1,26 +1,27 @@
-{
-    type Todo = {
-        title: string;
-        description: string;
-        label: string;
-        priorty: 'height' | 'low';
-    }
+    // PartialType 부분적 수정  
 
-    //부분 수정
-    function updateTodo(todo: Todo, fieldsToUpdate: Partial<Todo>): Todo {
-        return {
-            ...todo,
-            ...fieldsToUpdate
+    {
+        type ToDo = {
+            title: string;
+            description: string;
+            label: string;
+            priority: 'high' | 'low';
         };
-    }
+    
+        function updateTodo(todo: ToDo, fieldsToUpdate: Partial<ToDo>): ToDo {
+            return {
+                ...todo, ...fieldsToUpdate
+            }
+        }
 
-    const todo: Todo = {
-        title: 'learn TypeScript',
-        description: 'study hard',
-        label: 'study',
-        priorty: 'height'
-    }
+        const todo: ToDo = {
+            title: 'learn TypeScript',
+            description: 'study hard',
+            label: 'study',
+            priority: 'high'
+        };
 
-    const updated = updateTodo(todo, {priorty: 'low'});
-    console.log(updated);
-}
+        console.log(todo);
+        const update = updateTodo(todo, { title: "check"});
+        console.log(update);
+    }
