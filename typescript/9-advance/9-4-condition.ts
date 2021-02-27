fbddb13f58dbfb1f614758cc7f94a0acf060b74b
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2021-02-27 23:50:51 +0900

    Ts condition type 재복습

--- typescript/9-advance/9-4-condition.ts
+++ typescript/9-advance/9-4-condition.ts
@@ -1,8 +1,8 @@
-type Check<T> = T extends string? boolean : number;
-type Type = Check<string>; // boolean type
+type Check<T> = T extends string ? boolean : number;
 
-//조건적
-type TypeName<T> = T extends string
+type Type = Check<string>;
+
+type Typename<T> = T extends string 
     ? 'string'
     : T extends number
     ? 'number'
@@ -12,8 +12,4 @@ type TypeName<T> = T extends string
     ? 'undefined'
     : T extends Function
     ? 'function'
-    : 'object'
-
-    type T0 = TypeName<string>; 'string'
-    type T1 = TypeName<'a'>; 'string' 
-    type T2 = TypeName<() => void>; 'function'
+    : 'object';
