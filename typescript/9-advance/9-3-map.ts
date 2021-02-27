e15b03357abfa34808972d95bef87b4ecdc1d759
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2021-02-27 23:42:45 +0900

    Ts map type 재복습

--- typescript/9-advance/9-3-map.ts
+++ typescript/9-advance/9-3-map.ts
@@ -2,64 +2,46 @@
     type Video = {
         title: string;
         author: string;
+        description: string;
     }
-    
-    type Optional<T> = {
-        [P in keyof T]?: T[P]  // for...in
-    }
-    
-    type ReadOnly<T> = {
-        readonly [P in keyof T]?: T[P];
-    } 
-    
+    // type VideoOptional = {
+    //     title?: string;
+    //     author?: string;
+    //     description?: string;
+    // }
+
+    // type VideoReadOnluy = {
+    //     readonly title: string;
+    //     readonly author: string;
+    //     readonly description: string;
+    // }
+   
+    type Optional<T> =  {
+       [P in keyof T]?: T[P]
+   }
+
+   type ReadOnly<T> = {
+    readonly [P in keyof T] : T[P]
+   }
+
     type VideoOptional = Optional<Video>;
-    const videoOp: VideoOptional = {
-        title: '7번방 의선물'
-    }
-    
+
     type Animal = {
         name: string;
         age: number;
     }
-    
-    type AnimalOptional = Optional<Animal>;
-    const animalOp: AnimalOptional = {
-        name: '초롱이',
-    }
-    
-    animalOp.name = '짱돌';
-    
-    const video: ReadOnly<Video> = {
-        title: 'hi',
-        author: 'JONG'
-    }
-    
-    // video.author = 'JJONG' // error
-    // type VideoOptional = {
-    //     title?: string;
-    //     author?: string;
-    // }
-    
-    // type VideoReadOnly = {
-    //     readonly title: string;
-    //     readonly author: string;
-    // }
-    type Nullable<T> = {
-        [P in keyof T]: T[P] | null;
+
+    const animal: Optional<Animal> = {
+        name: 'dog',
     }
 
-    const obj2: Nullable<Video> = {
+    type Nullable<T> = {
+        [P in keyof T]: T[P] | null
+    }
+
+    const obj1: Nullable<Video> = {
         title: null,
         author: null,
+        description: null,
     }
-
-    type Proxy<T> = {
-        get(): T;
-        set(value: T): void;
-    };
-
-    type Proxify<T> = {
-        [P in keyof T]: Proxy<T[P]>;
-    }
-}
-
+ }
