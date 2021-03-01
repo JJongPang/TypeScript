fa270ce1fa1f5b7deca8631b56e77071069c9372
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2021-03-01 11:28:23 +0900

    Ts Pick 기능 확인

--- typescript/9-advance/9-7-pick.ts
+++ typescript/9-advance/9-7-pick.ts
@@ -4,24 +4,23 @@
         title: string;
         url: string;
         data: string;
-    };
+    }
+
+    type VideoMetadata  =  Pick<Video, 'id' | 'title'>
 
     function getVideo(id: string): Video {
         return {
             id,
             title: 'video',
-            url: 'https://..',
-            data: 'byte-data..'
-        }
+            url: 'https://',
+            data: 'byte-data...'
+        };
     }
-
-    // 기존의 타입에서 원하는 데이타만 쏙쏙 가지고 올 때
-    type VideoMetadata = Pick<Video, 'id' | 'title'>
 
     function getVideoMetadata(id: string): VideoMetadata {
         return {
-            id,
-            title: 'title'
+            id: id,
+            title: 'title',
         }
     }
 }
