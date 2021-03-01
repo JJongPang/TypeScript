1efec9e59455ac9b8ec8248f67e03a2604e6a4e2
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2021-03-01 14:08:34 +0900

    Js prototype 재복습

--- typescript/10-javascript/10-1-proto.js
+++ typescript/10-javascript/10-1-proto.js
@@ -4,33 +4,38 @@ const y = {};
 console.log(x);
 console.log(y);
 console.log(x.toString());
+
 console.log(x.__proto__ === y.__proto__);
 
 const array = [];
 console.log(array);
 
-console.clear();
-
-function CoffeeMachin(beans) {
+function CoffeeMachine(beans) {
     this.beans = beans;
 
-    // Instace member level
+    // Instance member level
+
     // this.makeCoffee = (shots) => {
-    //     console.log('making...');
+    //     console.log('making...Coffee~!!!!');
     // };
 }
 
-// Prototype member level
-CoffeeMachin.prototype.makeCoffee = (shots) => {
-    console.log('makeing');
+CoffeeMachine.prototype.makeCoffee = (shots) => {
+    console.log('makeing coffee...');
 };
+
+const machine1 = new CoffeeMachine(10);
+const machine2 = new CoffeeMachine(20);
+
+console.log(machine1);
+console.log(machine2);
 
 function LatteMachine(milk) {
     this.milk = milk;
 }
 
-LatteMachine.prototype = Object.create(CoffeeMachin.prototype);
+// CoffeeMachine 상속
+LatteMachine.prototype = Object.create(CoffeeMachine.prototype);
 
 const latteMachine = new LatteMachine(123);
 console.log(latteMachine);
-latteMachine.makeCoffee();
