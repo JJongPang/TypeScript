0ba98e2a573fa24ca180ee7bb33550b5e95d4d24
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2021-03-01 17:01:46 +0900

    Ts 디버깅 하는 방법

--- typescript/11-config/src/main.ts
+++ typescript/11-config/src/main.ts
@@ -1,1 +1,13 @@
-console.log('moo ya ho')
+'use strict';
+
+class Car {
+    engine = 0;
+    move() {
+        const engine = this.engine + 1;
+        console.log('engine');
+        console.log(engine);
+    }
+}
+
+const car = new Car();
+car.move();
